523668bc7f0b4a8d8ace4177c506d46b909a8bd4
Kamil <moskalakamil07@gmail.com>
2024-03-21 23:43:01 +0100

    refactor: update theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -12,45 +12,51 @@ export const getColors = (colorName: keyof typeof colors) => {
 
 export const lightTheme = createTheme({
   colors: flatColors,
-  spacing: {
-    "0": 0,
-    "0.5": 2,
-    "1": 4,
-    '1.5': 6,
-    "2": 8,
-    '2.5': 10,
-    "3": 12,
-    '3.5': 14,
-    "4": 16,
-    "5": 20,
-    "6": 24,
-    "7": 28,
-    "8": 32,
-    "9": 36,
-    "10": 40,
-    "11": 44,
-    "12": 48,
-    "14": 56,
-    "16": 64,
-    "20": 80,
-    "24": 96,
-    "28": 112,
-    "32": 128,
-    "36": 144,
-    "40": 160,
-    "44": 176,
-    "48": 192,
-    "52": 208,
-    "56": 224,
-    "60": 240,
-    "64": 256,
-    "72": 288,
-    "80": 320,
-    "96": 384,
+  borderRadii: {
+    small: 12,
+    big: 24,
   },
+  spacing: {},
   breakpoints: {
     phone: 0,
     tablet: 768,
+  },
+  textVariants: {
+    h1: {
+      fontFamily: "Poppins-Bold",
+      fontSize: 36,
+    },
+    h2: {
+      fontFamily: "Poppins-Bold",
+      fontSize: 28,
+    },
+    h3: {
+      fontFamily: "Poppins-Bold",
+      fontSize: 22,
+    },
+    h4: {
+      fontFamily: "Poppins-SemiBold",
+      fontSize: 16,
+    },
+    btn: {
+      fontFamily: "Poppins-SemiBold",
+      fontSize: 16,
+      spacing: "1%",
+    },
+    lbl: {
+      fontFamily: "Poppins-Medium",
+      fontSize: 16,
+    },
+    input: {
+      fontFamily: "Poppins-Medium",
+      fontSize: 15,
+      lineHeight: 20,
+    },
+    caption: {
+      fontFamily: "Poppins-Regular",
+      fontSize: 10,
+      lineHeight: 10,
+    },
   },
 });
 
