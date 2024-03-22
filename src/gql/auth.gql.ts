8343d207a1d0985e040a98fa72ff032b33d06a5e
Kamil <moskalakamil07@gmail.com>
2024-03-22 23:15:30 +0100

    refactor: rename files

--- src/gql/auth.gql.ts
+++ src/gql/auth.gql.ts
@@ -1,11 +1,6 @@
 import {gql} from "@src/__generated__";
 
 export namespace AuthApi {
-  export interface LoginReq {
-    email: string;
-    password: string;
-  }
-
   export const MUTATION_LOGIN = gql(/* GraphQL */ `
     mutation Login($email: String!, $password: String!) {
       loginUser(email: $email, password: $password) {
